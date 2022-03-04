47d71c40eb51454be5b1b2e669e4feeae57e0955
Rinasham <rinashamau@gmail.com>
2022-03-04 20:49:32 +1100

    [add] innerHTML/textContent/innerText

---
+++ innerHTML/script.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById('text-area').textContent)
+console.log(document.getElementById('text-area').innerText)
+console.log(document.getElementById('text-area').innerHTML)
+console.log(typeof document.getElementById('text-area').innerHTML)
+console.log(typeof document.getElementById('text-area'))
+
+console.log(document.getElementById('text4').textContent = 'textContent\nです')
+console.log(document.getElementById('text5').innerText = 'innerText\nです')
+console.log(document.getElementById('text6').innerHTML = 'innerHTML\nです')
